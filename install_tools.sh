4a0fe2438bc29dba9aa0bbe60ae2be8e55f6cb00
robot527 <luoyaozu@foxmail.com>
2015-08-20 18:51:54 +0800

    新增 install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# install pip for python2
+sudo easy_install pip
+
+sudo apt-get install -y python3
+
+# install pip for python3
+sudo easy_install3 pip
+
+sudo apt-get install -y ipython
+
+# install windows enviroment
+sudo apt-get install -y wine
+
+# install graphic vim editor
+sudo apt-get install -y vim-gnome
+
+# install file/dir compare tool
+sudo apt-get install -y meld
+
+# install screenshot tool
+sudo apt-get install -y shutter
+
+
+
