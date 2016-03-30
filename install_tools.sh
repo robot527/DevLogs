fb92164b7f45182d979b9893a44b8f304089d369
robot527 <luoyaozu@foxmail.com>
2016-03-30 18:22:27 +0800

    Modified install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -1,17 +1,22 @@
 #! /bin/bash
 
-# install pip for python2
+
+sudo apt-get install -y git
+
+sudo apt-get install -y ibus-googlepinyin
+
+# install python-setuptools and pip for python2
+sudo apt-get install -y python-setuptools
 sudo easy_install pip
 
 sudo apt-get install -y python3
 
-# install pip for python3
+# install python3-setuptools and pip for python3
+sudo apt-get install -y python3-setuptools
 sudo easy_install3 pip
 
 sudo apt-get install -y ipython
 
-# install windows enviroment
-sudo apt-get install -y wine
 
 # install graphic vim editor
 sudo apt-get install -y vim-gnome
@@ -23,4 +28,11 @@ sudo apt-get install -y meld
 sudo apt-get install -y shutter
 
 
+#sudo apt-get install -y ubuntu-desktop
+
+
+sudo apt-get install -y retext
+
+sudo apt-get install -y cscope
+sudo apt-get install -y ctags
 
